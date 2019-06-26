1a57c4adca38bf85ff823e842699f3737c2e9a0c
Fasith <hybridninja007@gmail.com>
2019-06-26 11:15:51 +0530

    Final Commit on successful project completion

---
+++ OpenStreetMap_RoutePlanner/src/route_planner.h
@@ -0,0 +1,33 @@
+#ifndef ROUTE_PLANNER_H
+#define ROUTE_PLANNER_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "route_model.h"
+
+
+class RoutePlanner {
+  public:
+    RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
+    // Add public variables or methods declarations here.
+	
+  float GetDistance() { return distance;}
+  void AStarSearch();
+  
+  private:
+    // Add private variables or methods declarations here.
+    RouteModel &m_Model;
+   	RouteModel::Node *start_node;
+    RouteModel::Node *end_node;
+  	float distance;
+  	std::vector<RouteModel::Node *> open_list;
+  	RouteModel::Node *NextNode();
+  
+  	float CalculateHValue(RouteModel::Node *node);
+  	void AddNeighbors(RouteModel::Node *current_node);
+  
+  	std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node *);
+};
+
+#endif
